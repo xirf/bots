ba2a201b3b4393c8fcad164029639ddae7e2cbbd
evaasmakula <andikacahargen2@gmail.com>
2021-12-02 19:32:41 +0700

    fixing instalation

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-pkg install nodejs-lts git tesseract libwebp wget imagemagick ffmpeg -y
-wget https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/master/ind.traineddata
-mv ind.traineddata /data/data/com.termux/files/usr/share/tessdata 
+pkg install nodejs-lts git libwebp wget imagemagick ffmpeg -y
 npm install -g pnpm
 pnpm install
